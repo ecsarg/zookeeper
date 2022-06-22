cf4c3e6f4e23b4abee6df341aca0d35ee87564aa
Ellie Sargent <elliecsargent@gmail.com>
2022-06-22 18:45:06 -0500

    found error for posting

--- server.js
+++ server.js
@@ -6,14 +6,11 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// parse incoming string or array data
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
 
-app.use(express.static('public'));
-
-function filterByQuery(query, animalsArray) {
+function filterByQuery(query, animalsArray) { 
   let personalityTraitsArray = [];
   let filteredResults = animalsArray;
   if (query.personalityTraits) {
@@ -46,29 +43,29 @@ function findById(id, animalsArray) {
 }
 
 function createNewAnimal(body, animalsArray) {
-    const animal = body;
-    animalsArray.push(animal);
-    fs.writeFileSync(
-        path.join(__dirname, './data/animals.json'),
-        JSON.stringify({ animals: animalsArray }, null, 2)
-    );
-    return animal;
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
 }
 
 function validateAnimal(animal) {
-    if (!animal.name || typeof animal.name !== 'string') {
-        return false;
-    }
-    if (!animal.species || typeof animal.species !== 'species') {
-        return false;
-    }
-    if (!animal.diet || typeof animal.diet !== 'diet') {
-        return false;
-    }
-    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-        return false;
-    }
-    return true;
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
 }
 
 app.get('/api/animals', (req, res) => {
@@ -89,17 +86,15 @@ app.get('/api/animals/:id', (req, res) => {
 });
 
 app.post('/api/animals', (req, res) => {
-    // set id based on what the next index of the array will be
-    req.body.id = animals.length.toString();
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
 
-    // if any data in req.body is incorrect, send 400 error back
-    if (!validateAnimal(req.body)) {
-        res.status(400).send('The animal is not properly formatted.');
-    } else {
-    // add animal to json file and animals array in this function
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
     const animal = createNewAnimal(req.body, animals);
-    res.json(req.body);
-    }
+    res.json(animal);
+  }
 });
 
 app.get('/', (req, res) => {
@@ -111,7 +106,7 @@ app.get('/animals', (req, res) => {
 });
 
 app.get('/zookeepers', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeerps.html'));
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
 });
 
 app.get('*', (req, res) => {
